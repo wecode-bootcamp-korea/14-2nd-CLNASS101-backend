c9e608738ff9fa3aa4bd64f7072c3ff51c88dd65
dev-swd <tladnjsen@gmail.com>
2020-12-08 21:27:29 +0900

    [CLNASS_101:심원두] Add ProductDetailView with UnitTest.

--- core/utils.py
+++ core/utils.py
@@ -7,23 +7,33 @@ from django.http import JsonResponse
 from my_settings import SECRET, ALGORITHM
 from user.models import User
 
+def login_decorator(view_name):
+    def real_decorator(func):
+        def wrapper(self, request, *args, **kwargs):
+            try:
+                token   = request.headers.get('Authorization', None)
 
-def login_decorator(func):
-    def wrapper(self, request, *args, **kwargs):
-        try:
-            token = request.headers.get('Authorization', None)
-            payload = jwt.decode(
-                token, SECRET['secret'], algorithm=ALGORITHM['algorithm'])
-            user = User.objects.get(id=payload['user_id'])
-            request.user = user
+                if not token and view_name == 'ProductDetailView':
+                    request.user = User.objects.filter(id=0)
+                    return func(self, request, *args, **kwargs)
 
-        except jwt.exceptions.DecodeError:
-            return JsonResponse({"MESSAGE": "INVALID_TOKEN"}, status=400)
-        except User.DoesNotExist:
-            return JsonResponse({"MESSAGE": "INVALID_USER"}, status=401)
-        return func(self, request, *args, **kwargs)
-    return wrapper
+                payload = jwt.decode(
+                    token,
+                    SECRET['secret'],
+                    algorithm=ALGORITHM['algorithm']
+                )
+                user = User.objects.get(id=payload['user_id'])
+                request.user = user
 
+            except jwt.exceptions.DecodeError:
+                return JsonResponse({"MESSAGE": "INVALID_TOKEN"}, status=400)
+
+            except User.DoesNotExist:
+                return JsonResponse({"MESSAGE": "INVALID_USER"}, status=401)
+
+            return func(self, request, *args, **kwargs)
+        return wrapper
+    return real_decorator
 
 def get_hashed_pw(password):
     return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt()).decode("UTF-8")
---
+++ product/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from product.views import ProductDetailView
+
+urlpatterns = [
+    path('/<int:product_id>', ProductDetailView.as_view(), name='products')
+]
