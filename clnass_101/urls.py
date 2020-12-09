87019d5e77e610a582a3e73f15827722891be4cf
dev-swd <tladnjsen@gmail.com>
2020-12-09 23:26:22 +0900

    [CLNASS_101:심원두] Add SelectProductAndPaymentView with UnitTest.

--- clnass_101/urls.py
+++ clnass_101/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('user', include('user.urls')),
     path('products', include('product.urls')),
+    path('products/<int:product_id>', include('order.urls'))
 ]
